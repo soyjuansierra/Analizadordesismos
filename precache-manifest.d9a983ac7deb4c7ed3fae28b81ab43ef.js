4b2464269f2093b0e5d5e6f708af4c7f0534ab3f
kboul <kos.voulgaridis@gmail.com>
2019-09-18 16:23:02 +0300

    Updates

---
+++ precache-manifest.d9a983ac7deb4c7ed3fae28b81ab43ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3da963d5d91b0170a135dc8c8bcdb69",
+    "url": "/react-earthquake/index.html"
+  },
+  {
+    "revision": "4ded68e7b3fb4497b7e8",
+    "url": "/react-earthquake/static/css/2.d6f34132.chunk.css"
+  },
+  {
+    "revision": "1c55630ef3344e58435d",
+    "url": "/react-earthquake/static/css/main.1b9804dd.chunk.css"
+  },
+  {
+    "revision": "4ded68e7b3fb4497b7e8",
+    "url": "/react-earthquake/static/js/2.42ea5e77.chunk.js"
+  },
+  {
+    "revision": "1c55630ef3344e58435d",
+    "url": "/react-earthquake/static/js/main.8c3ebf1c.chunk.js"
+  },
+  {
+    "revision": "d8ec8e5d15895cfca501",
+    "url": "/react-earthquake/static/js/runtime~main.a0e5b94d.js"
+  }
+]);
